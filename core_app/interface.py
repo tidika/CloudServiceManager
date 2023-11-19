046749b36146b864f4faa0a2a77cd2d57e6e89ff
tidika <tochi.idika@gmail.com>
2023-11-19 13:57:17 -0600

    updated files

--- core_app/interface.py
+++ core_app/interface.py
@@ -44,17 +44,4 @@ class Client:
         return rds
 
 
-class Resource:
-    def __init__(self, access_key, secret_key, region):
-        self.access_key = access_key
-        self.secret_key = secret_key
-        self.region = region
 
-    def ec2_resource(self):
-        ec2 = boto3.resource(
-            "ec2",
-            aws_access_key_id=self.access_key,
-            aws_secret_access_key=self.secret_key,
-            region_name=self.region,
-        )
-        return ec2
